1b2f264a01940885de2206fcb09fb74bab80f172
BernhardWerner <werner@ma.tum.de>
2020-12-10 13:06:14 +0100

    Added existing library + copies

---
+++ egdodAnim.js
@@ -0,0 +1,32 @@
+/*
+ * Put <script src="egdod.js" data-scriptid="YOURINITSCRIPTID"></script> before your createCindy.
+ */
+(function(){
+	var code = document.createTextNode(`
+
+		// ************************************************************************************************
+		// Gets the ALICE and Toolbox colours.
+		// ************************************************************************************************
+
+
+
+`);
+
+
+	var scriptId = document.currentScript.dataset.scriptid;
+	if (!scriptId) scriptId = 'csinit';
+
+	var scriptElement = document.getElementById(scriptId);
+	if (!scriptElement) {
+		scriptElement = document.createElement("script");
+		scriptElement.id = scriptId;
+		scriptElement.type = "text/x-cindyscript";
+		document.head.appendChild(scriptElement);
+	}
+	if (scriptElement.firstChild) {
+		scriptElement.insertBefore(code, scriptElement.firstChild);
+	} else {
+		scriptElement.appendChild(code);
+	}
+
+})();
