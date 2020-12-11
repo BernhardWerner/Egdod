cfb7306994b0094c80fcc8923990cb496066543d
BernhardWerner <werner@ma.tum.de>
2020-12-11 16:02:58 +0100

    Implemented color conversion

--- egdodColor.js
+++ egdodColor.js
@@ -53,12 +53,61 @@
 		// ************************************************************************************************
 		// Color transformation
 		// ************************************************************************************************
-		rgb2hsl(vec) := (
+		rgb2hsv(vec) := (
+			regional(cMax, cMin, delta, maxIndex, h, s);
+
+			maxIndex = 1;
+			cMax = vec_1;
+			forall(2..3, 
+				if(vec_# > cMax,
+					maxIndex = #;
+					cMax = vec_#;
+				);
+			);
+
+			cMin = min(vec);
+			delta = cMax - cMin;
+
+			h =  if(delta ~= 0,
+					0;
+				,if(maxIndex == 1,
+					mod((vec_2 - vec_3) / delta, 6);
+				,if(maxIndex == 2,
+					2 + (vec_3 - vec_1) / delta
+				,if(maxIndex == 3,
+					4 + (vec_1 - vec_2) / delta
+					
+				)))) / 6;
+
+			s = if(cMax ~= 0, 0, delta / cMax);
+
+			[h, s, cMax];
 
 		);
 
-		hsl2rgb() := (
+		hsv2rgb(vec) := (
+			regional(c, x, m, r, g, b);
 
+			vec_1 = vec_1 * 6;
+			c = vec_2 * vec_3;
+			x = c * (1 - abs(mod(vec_1, 2) - 1));
+			m = vec_3 - c;
+
+			[r, g, b] =  if(vec_1 < 1, 
+							[c, x, 0];
+						,if(vec_1 < 2, 
+							[x, c, 0];
+						,if(vec_1 < 3, 
+							[0, c, x];
+						,if(vec_1 < 4, 
+							[0, x, c];
+						,if(vec_1 < 5, 
+							[x, 0, c];
+						,if(vec_1 < 6, 
+							[c, 0, x];
+						))))));
+
+			[r + m, g + m, b + m];
 		);
 		
 
