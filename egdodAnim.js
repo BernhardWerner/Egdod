b45947748bf50c5897be514e88c36ccb16083808
BernhardWerner <werner@ma.tum.de>
2021-02-04 16:43:03 +0100

    REmoved line object in favour of stroke objects + removed grid construction completely

--- egdodAnim.js
+++ egdodAnim.js
@@ -1,5 +1,5 @@
 /*
- * Put <script src="egdod.js" data-scriptid="YOURINITSCRIPTID"></script> before your createCindy.
+ * Put <script src="egdodAnim.js" data-scriptid="YOURINITSCRIPTID"></script> before your createCindy.
  */
 (function(){
 	var code = document.createTextNode(`
@@ -187,20 +187,6 @@
 	);
 
 
-
-	// ************************************************************************************************
-	// Line object needs
-	// start
-	// end
-	// color
-	// size
-	// arrow
-	// ************************************************************************************************
-	drawLineObject(obj) := (
-		draw([obj.start, obj.end], color->obj.color, size->obj.size, arrow->obj.arrow > 0.5, alpha->obj.alpha);
-	);
-
-
 	// ************************************************************************************************
 	// Circle object needs
 	// pos
@@ -246,19 +232,6 @@
 	
 
 	// ************************************************************************************************
-	// Draws quiver of line objects.
-	// ************************************************************************************************
-	drawQuiver(quiver) := forall(quiver, obj, drawLineObject(obj));
-
-
-	// ************************************************************************************************
-	// Draws grid.
-	// ************************************************************************************************
-	drawGrid(grid) := forall(grid, quiver, drawQuiver(quiver));
-
-
-
-	// ************************************************************************************************
 	// Cycles through flipbook of a flipbook object. Flipbook pages are equdistantly 
 	// spread across animation track as key frames.
 	// ************************************************************************************************
@@ -270,75 +243,6 @@
 	);
 
 
-	// ************************************************************************************************
-	// Animates every property of a line object that makes it appear.
-	// ************************************************************************************************
-	constructLineObject(obj, endPos, size, arrow, track) := (
-		tween(obj, "end", obj.start, endPos, track, "easeInOutCubic");
-		tween(obj, "alpha", 0, 1, track, "easeOutCirc");
-		tween(obj, "size", 0, size, track, "easeOutCirc");
-		if(arrow, tween(obj, "arrow", 0, 1, track, "easeInQuad"));
-	);
-
-	// ************************************************************************************************
-	// Creates an "empty" line object which can be constructed afterwards.
-	// ************************************************************************************************
-	createRootLineObject(pos, color) := {
-		"start": pos,
-		"end":   0,
-		"color":  color,
-		"size":   0,
-	  "arrow":  0,
-	  "alpha":  0
-	};
-
-	// ************************************************************************************************
-	// Initialises quiver of lines.
-	// ************************************************************************************************
-	createRootQuiver(roots, color) := (
-		apply(roots,
-			createRootLineObject(#, color);	
-		);
-	);
-
-	// ************************************************************************************************
-	// Extracts start and end points from quiver.
-	// ************************************************************************************************
-	getQuiverStartPoints(quiver) := (
-		apply(quiver, obj, obj.start);
-	);
-	getQuiverEndPoints(quiver) := (
-		apply(quiver, obj, obj.end);
-	);
-
-	// ************************************************************************************************
-	// Animates every property of the line objects of a quiver via an animation cascade.
-	// ************************************************************************************************
-	constructQuiver(quiver, targets, size, arrow, cascade) := (
-		forall(1..length(quiver),
-		constructLineObject(quiver_#, targets_#, size, arrow, cascade_#);
-		);
-		);
-		
-	// ************************************************************************************************
-	// Setting up grid lines.
-	// ************************************************************************************************
-	createRootGrid(pos, xOffset, yOffset, hNumber, vNumber, vDist, hDist, color) := (
-		[
-			createRootQuiver(apply(0..hNumber - 1, pos + [# * hDist, yOffset]), color),
-			createRootQuiver(apply(0..vNumber - 1, pos + [xOffset, # * vDist]), color)
-		];
-	);
-
-	// ************************************************************************************************
-	// Creating a grid.
-	// ************************************************************************************************
-	constructGrid(grid, hLength, vLength, size, arrow, hCascade, vCascade) := (
-		constructQuiver(grid.x, apply(getQuiverStartPoints(grid.x), # + [0, vLength]), size, arrow, hCascade);
-		constructQuiver(grid.y, apply(getQuiverStartPoints(grid.y), # + [hLength, 0]), size, arrow, vCascade);
-		);
-		
-		
 		
 	strokeSampleRateEABOW = 256;
 	// ************************************************************************************************
