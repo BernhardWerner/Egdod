6713e9098f12efed4162fb3b4e6ca8b9c3ba8ac5
Bernhard Werner <bernhard.o.werner@web.de>
2021-08-25 22:23:15 +0200

    Implemented canvas information

--- plugin/egdod.js
+++ plugin/egdod.js
@@ -34,9 +34,40 @@ CindyJS.registerPlugin(1, "egdod", function(api) {
         return {ctype: "number", value: {real: x, imag: 0}}
     }
 
+    function cList(arr) {
+        return {ctype: "list", value: arr}
+    }
+
+    function cPoint(arr) {
+        return cList([cReal(arr[0]), cReal(arr[1])])
+    }
+
+
+
+
     ////////////////////////////////////////////////////////////////////////////////////////////////////
     // CindyScript functions
     ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // *************************************************************************************************
+    // Prints ctype of variable
+    // *************************************************************************************************
+    defOp("debugCtype", 1, function(args, modifs) {
+        return {
+            ctype: "string",
+            value: evaluate(args[0]).ctype
+        }
+    });
+
+    // *************************************************************************************************
+    // Prints value of variable
+    // *************************************************************************************************
+    defOp("debugValue", 1, function(args, modifs) {
+        return {
+            ctype: "string",
+            value: evaluate(args[0]).value
+        }
+    });
 
 
     // *************************************************************************************************
@@ -47,6 +78,32 @@ CindyJS.registerPlugin(1, "egdod", function(api) {
         if(x.ctype == "number" && x.value.imag == 0) return cReal(Math.sign(x.value.real));
         console.log("Type missmatch in sign.");
         return nada;
+    });
+
+
+
+    // *************************************************************************************************
+    // Information about the canvas.
+    // *************************************************************************************************
+    defOp("canvas", 0, function(args, modis) {
+        let points = call("screenbounds", [], {}).value.map(p => [p.value[0].value.real, p.value[1].value.real]);
+
+        return {
+            ctype: "JSON",
+            value: {
+                tl: cPoint(points[0]),
+                tr: cPoint(points[1]),
+                br: cPoint(points[2]),
+                bl: cPoint(points[3]),
+                center: cPoint([0.5 * (points[0][0] + points[2][0]), 0.5 * (points[0][1] + points[2][1])]),
+                width: cReal(points[1][0] - points[0][0]),
+                height: cReal(points[1][0] - points[2][0]),
+                left: cReal(points[0][0]),
+                top: cReal(points[0][1]),
+                right: cReal(points[2][0]),
+                bottom: cReal(points[2][1])
+            }
+        }
     });
 
 });
