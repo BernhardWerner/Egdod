f9a82bd720c8d5b720e2c34cfb3d82d3e0cd5832
Bernhard Werner <bernhard.o.werner@web.de>
2021-08-26 22:11:10 +0200

    Added time keeping functionality; still buggy after load

--- plugin/egdod.js
+++ plugin/egdod.js
@@ -20,9 +20,11 @@ CindyJS.registerPlugin(1, "egdod", function(api) {
 
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Helper functions
+    // Helper variables and functions
     ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+    // Calls a Cindyscript function
     function call(name, args, modifs) {
         return evaluate({
             ctype : "function",
@@ -32,26 +34,97 @@ CindyJS.registerPlugin(1, "egdod", function(api) {
         });
     }
 
+    // Return Cindy-compliant variables
     function cReal(x) {
         return {ctype: "number", value: {real: x, imag: 0}}
     }
 
-    function cBool(x) {
-        return {ctype: "boolean", value: x}
+    function cBool(bool) {
+        return {ctype: "boolean", value: bool}
     }
 
     function cList(arr) {
         return {ctype: "list", value: arr}
     }
 
-    function cPoint(arr) {
-        return cList([cReal(arr[0]), cReal(arr[1])])
+    function cJSON(dict) {
+        return {ctype: "JSON", value: dict}
     }
 
-    function xy(p) {
-        if(p.ok) return [p.x, p.y]
+    function cPoint(arr) {
+        return cList(arr.map(x => cReal(x)))
+    }
+
+    // Turns point into list of homogeneous coordinates
+    function homog(p) {
+        if(p.ok) return [p.x, p.y, 1]
         return nada
     }
+
+    function canvasToCindy(px, py) {
+        var virtualwidth = 0;
+        var virtualheight = 0;
+
+        if(api.config.ports) {
+            var port = api.config.ports;
+
+            if (port.virtualwidth) virtualwidth = port.virtualwidth;
+            if (port.virtualheight) virtualheight = port.virtualheight;
+        }
+
+        
+        vscale = 1;
+
+        if (virtualwidth || virtualheight) {
+            var canvas = api.instance.canvas;
+
+            vscale = Math.max(
+                virtualwidth ? virtualwidth / canvas.width : 0,
+                virtualheight ? virtualheight / canvas.height : 0
+            );
+        }
+
+
+
+        var matrix = api.getInitialMatrix();
+        var xx = px * vscale - matrix.tx;
+        var yy = py * vscale + matrix.ty;
+        var x = (xx * matrix.d - yy * matrix.b) / matrix.det;
+        var y = -(-xx * matrix.c + yy * matrix.a) / matrix.det;
+        return [x, y, 1];
+    }
+
+    function getCanvasCoordinates() {
+        var canvas = api.instance.canvas;
+
+        return [canvasToCindy(0, canvas.clientHeight), canvasToCindy(canvas.clientWidth, 0)];
+    }
+
+    function computerSeconds() {
+        var date = new Date()
+        var time = date.getTime();
+
+        return time / 1000;
+    }
+
+    
+    let timeBuffer = 0;
+    let scriptStartTime = 0;
+    
+
+    function now() {
+        return computerSeconds() - scriptStartTime;
+    }
+
+
+    function trackStarted(track, delay = 0) {
+        return now() >= track.start.value.real + delay;
+    }
+
+
+
+
+
 
 
 
@@ -85,7 +158,7 @@ CindyJS.registerPlugin(1, "egdod", function(api) {
     // Gives the sign of a real numer or a list of real numbers as 1, 0 or +1
     // *************************************************************************************************
     defOp("sign", 1, function(args, modifs) {
-        let x = evaluate(args[0]);   
+        var x = evaluate(args[0]);   
         if(x.ctype == "number" && x.value.imag == 0) return cReal(Math.sign(x.value.real));
         console.log("Type missmatch in sign.");
         return nada;
@@ -97,24 +170,23 @@ CindyJS.registerPlugin(1, "egdod", function(api) {
     // Information about the canvas.
     // *************************************************************************************************
     defOp("canvas", 0, function(args, modis) {
-        let points = call("screenbounds", [], {}).value.map(p => extractPoint(p));
+        
 
-        return {
-            ctype: "JSON",
-            value: {
-                tl: cPoint(xy(points[0])),
-                tr: cPoint(xy(points[1])),
-                br: cPoint(xy(points[2])),
-                bl: cPoint(xy(points[3])),
-                center: cPoint([0.5 * (points[0].x + points[2].x), 0.5 * (points[0].y + points[2].y)]),
-                width: cReal(points[1].x - points[0].x),
-                height: cReal(points[1].x - points[2].x),
-                left: cReal(points[0].x),
-                top: cReal(points[0].y),
-                right: cReal(points[2].x),
-                bottom: cReal(points[2].y)
-            }
-        }
+        var points = getCanvasCoordinates();
+
+        return cJSON({
+            tl: cPoint([points[0][0], points[1][1]]),
+            tr: cPoint(points[1]),
+            br: cPoint([points[1][0], points[0][1]]),
+            bl: cPoint(points[0]),
+            center: cPoint([0.5 * (points[0][0] + points[1][0]), 0.5 * (points[0][1] + points[1][1]), 1]),
+            width: cReal(points[1][0] - points[0][0]),
+            height: cReal(points[1][1] - points[0][1]),
+            left: cReal(points[0][0]),
+            top: cReal(points[1][1]),
+            right: cReal(points[1][0]),
+            bottom: cReal(points[0][1])
+        });
     });
 
 
@@ -122,32 +194,89 @@ CindyJS.registerPlugin(1, "egdod", function(api) {
     // Sets up an animation track with given start and end time.
     // *************************************************************************************************
     defOp("setupAnimationTrack", 2, function(args, modifs) {
-        let s = evaluate(args[0]).value.real;
-        let e = evaluate(args[1]).value.real;
+        var s = evaluate(args[0]).value.real;
+        var e = evaluate(args[1]).value.real;
 
-        let looping = modifs.hasOwnProperty("looping") ? evaluate(modifs.looping) : cBool(false);
+        var looping = modifs.hasOwnProperty("looping") ? evaluate(modifs.looping) : cBool(false);
         
         
-        return {
-            ctype: "JSON",
-            value: {
-                start:    cReal(s),
-                end:      cReal(e),
-                duration: cReal(e - s),
-                timeLeft: cReal(e - s),
-                progress: cReal(0),
-                running:  cBool(true),
-                looping:  looping
-            }
-        }
+        return cJSON({
+            start:    cReal(s),
+            end:      cReal(e),
+            duration: cReal(e - s),
+            timeLeft: cReal(e - s),
+            progress: cReal(0),
+            running:  cBool(true),
+            looping:  looping
+        });
     });
 
+    // *************************************************************************************************
+    // Updates animation track with delta time variable.
+    // *************************************************************************************************
     defOp("updateAnimationTrack", 2, function(args, modifs) {
-        let track = evaluate(args[0]).value;
-        let delta = evaluate(args[1]).value.real;
+        var track = evaluate(args[0]).value;
+        var delta = evaluate(args[1]).value.real;
 
-        track.duration.value.real -= delta;
-       
+        if(track.running.value && trackStarted(track)) {
+            track.timeLeft.value.real -= delta;
+
+            if(track.timeLeft.value.real <= 0) {
+                if(track.looping.value) {
+                    track.timeLeft.value.real = track.end.value.real - track.start.value.real;
+                } else {
+                    track.timeLeft.value.real = 0;
+                    track.running.value = false;
+                }
+            };
+            track.progress.value.real = 1 - track.timeLeft.value.real / track.duration.value.real;
+
+        };
     });
+
+
+
+
+    // *************************************************************************************************
+    // Computes time of last frame. 
+    // *************************************************************************************************
+    defOp("deltaTime", 0, function(args, modifs) {
+        var result = computerSeconds() - timeBuffer;
+        timeBuffer = computerSeconds();
+
+        return cReal(result);
+    });
+
+    // *************************************************************************************************
+    // Time since start. 
+    // *************************************************************************************************
+    defOp("now", 0, function(args, modifs) {
+        return cReal(now());
+    });
+
+    // *************************************************************************************************
+    // Sets up time variables. 
+    // *************************************************************************************************
+    defOp("setupTime", 0, function(args, modifs) {
+        timeBuffer = computerSeconds();
+        scriptStartTime = timeBuffer;
+    });
+
 
 });
+
+
+
+
+// if(track.running & trackStarted(track),
+//     track.timeLeft = track.timeLeft - delta;	
+//     track.progress = 1 - track.timeLeft / track.duration;
+//     if(track.timeLeft <= 0,
+//         if(track.looping,
+//             track.timeLeft = track.end - track.start;
+//         , // else //
+//             //track.timeLeft = 0;
+//             //track.running = false;		
+//         );
+//     );
+// );
